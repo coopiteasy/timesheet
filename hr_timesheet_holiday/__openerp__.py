0dc1d1364cbff885c48c40387b4eac4b7990f62e
robin.keunen <robin@keunen.net>
2020-02-05 11:03:44 +0100

    [REF] hr_timesheet_sheet_restrict_analytic: lint js

--- hr_timesheet_holiday/__openerp__.py
+++ hr_timesheet_holiday/__openerp__.py
@@ -5,6 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
+    # touch
     "name": "Link holidays to analytic lines",
     "version": "9.0.1.0.0",
     "category": "Generic Modules/Human Resources",
